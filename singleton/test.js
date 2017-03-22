fb635c16cf0ebbbd71e17ec2fd85d2cdb0f59386
yulu.cui <1281993444@qq.com>
2017-03-22 15:31:03 +0800

    tsc 配置文件

--- singleton/test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var hunger_1 = require("./hunger");
-var lazy_1 = require("./lazy");
-//返回true 说明是同一个实例
-console.log(hunger_1.Hunger.getInstance() == hunger_1.Hunger.getInstance());
-console.log(lazy_1.Lazy.getInstance() == lazy_1.Lazy.getInstance());
